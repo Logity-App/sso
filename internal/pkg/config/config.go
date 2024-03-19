50b5d23f34d9850bcc053c29470a1284c48c3ece
lokk <lokk.is@dclouds.ru>
2024-03-19 18:26:02 +0700

    build

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -18,13 +18,13 @@ type AppConfig struct {
 }
 
 type GRPCCConfig struct {
-	Port    int           `env:"PORT" env-required:"44444"`
+	Port    int           `env:"PORT" env-required:"true"`
 	Timeout time.Duration `env:"TIMEOUT" env-default:"5s"`
 }
 
 type Database struct {
 	Enable          bool   `env:"POSTGRES_ENABLE" env-default:"false"`
-	Dsn             string `env:"POSTGRES_DSN" env-default:"postgres:5432"`
+	Dsn             string `env:"POSTGRES_DSN" env-required:"true" env-default:"postgres:5432"`
 	MaxIdleConn     int    `env:"POSTGRES_MAX_IDLE_CONN" env-default:"3"`
 	MaxLifeTimeConn int    `env:"POSTGRES_LIFETIME_CONN" env-default:"3"`
 }
