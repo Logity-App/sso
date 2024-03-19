ccb5e167e1b8b22b3b89535467500b9c87e4b06a
lokk <lokk.is@dclouds.ru>
2024-03-19 17:23:41 +0700

    create network

--- .github/deploy/deploy.sh
+++ .github/deploy/deploy.sh
@@ -2,6 +2,11 @@
 IMAGE_NAME=$DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
 CONTAINER_NAME=$DOCKER_REPOSITORY_$DOCKER_TAG
 
+NETWORK_NAME=existing-network
+if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then
+     docker network create ${NETWORK_NAME} ;
+fi
+
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker-compose -f .github/deploy/docker-compose.${ENVIRONMENT}.yml down --remove-orphans
 docker-compose -f .github/deploy/docker-compose.${ENVIRONMENT}.yml pull
